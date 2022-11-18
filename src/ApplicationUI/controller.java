08952faee4baea21b935fca2bdd4232425f0474d
FarazRazi <farazrazi432@gmail.com>
2022-11-19 01:22:56 +0500

    First Commit - Project Setup sample window added

---
+++ src/ApplicationUI/controller.java
@@ -0,0 +1,17 @@
+package ApplicationUI;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class controller {
+
+    @FXML
+    private TextField fName;
+
+    @FXML
+    private TextField lName;
+
+    @FXML
+    private TextField rollNo;
+
+}
---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    private static Stage stg;
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            stg = primaryStage;
+            Parent root = FXMLLoader.load(getClass().getResource("ApplicationUI/welcome.fxml"));
+            Scene scene = new Scene(root, 600, 400);
+            primaryStage.setTitle("Managei");
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public void changeScene(String fxml) throws IOException {
+        Parent pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
